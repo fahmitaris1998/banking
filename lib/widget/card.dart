06439de85a7c7540017697a14962ed9be7262297
fahmitaris1998 <fahmitaris1998>
2021-01-31 06:15:06 +0700

    second

--- lib/widget/card.dart
+++ lib/widget/card.dart
@@ -10,29 +10,32 @@ class Card1 extends StatelessWidget {
   const Card1({Key key, this.idx, this.image, this.title1, this.title2}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.only(top: 6.0,left: 25.0,right: 25.0,bottom: 19.0),
-      margin: EdgeInsets.only(left: 25.0,top: 10,bottom: 20),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10.0),
-          boxShadow: [
-            BoxShadow(
-              color: Color.fromRGBO(53, 63, 222, 0.13),
-              spreadRadius: 2,
-              blurRadius:14,
-              offset: Offset(0, 5),
-            )
-          ]
-      ),
-      child: Column(
-        children: [
-          Image(image: ExactAssetImage(image)),
-          Text(title1),
-          Text(title2)
-        ],
-      ),
+    return ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 200.0),
+      child: Container(
+        padding: EdgeInsets.only(top: 6.0,left: 25.0,right: 25.0,bottom: 19.0),
+        margin: EdgeInsets.only(left: 25.0,top: 10,bottom: 20),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Color.fromRGBO(53, 63, 222, 0.13),
+                spreadRadius: 2,
+                blurRadius:14,
+                offset: Offset(0, 5),
+              )
+            ]
+        ),
+        child: Column(
+          children: [
+            Image(image: ExactAssetImage(image)),
+            Text(title1),
+            Text(title2)
+          ],
+        ),
 
+      ),
     );
   }
 }
